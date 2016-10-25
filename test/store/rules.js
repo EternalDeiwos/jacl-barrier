5e3cb05c044f713fbe12561b5819903621f050f5
Greg Linklater <greglink49@gmail.com>
2016-10-26 00:35:32 +0200

    test(Barrier): config and store for negative outcome integration tests

--- test/store/rules.js
+++ test/store/rules.js
@@ -53,6 +53,59 @@ module.exports = {
           }
         }
       }
+    },
+    deny: {
+      type: 'object',
+      required: ['subject', 'environment'],
+      properties: {
+        subject: {
+          type: 'object',
+          required: ['staff', 'department'],
+          properties: {
+            staff: {
+              type: 'boolean',
+              enum: [false]
+            },
+            department: {
+              type: 'string',
+              enum: ['Computer Science', 'Information Systems']
+            }
+          }
+        },
+        environment: {
+          type: 'object',
+          properties: {
+            time: {
+              type: 'object',
+              required: ['hours', 'minutes'],
+              anyOf: [
+                {
+                  properties: { 
+                    hours: {
+                      type: 'number',
+                      minimum: 7,
+                      maximum: 17
+                    },
+                    minutes: {
+                      type: 'number',
+                      minimum: 30
+                    }
+                  }
+                },
+                {
+                  properties: {
+                    hours: {
+                      type: 'number',
+                      maximum: 17,
+                      minimum: 8
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
     }
   }
 }
